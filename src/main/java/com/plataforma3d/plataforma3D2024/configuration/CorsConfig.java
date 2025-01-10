ba57566a74207c7b41f2d497cc1407ab66cb7499
Keny Pau Vas <keny_pauccarima@usmp.pe>
2025-01-09 23:14:02 -0500

    activando cors para el front correctamente

---
+++ src/main/java/com/plataforma3d/plataforma3D2024/configuration/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.plataforma3d.plataforma3D2024.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+
+    }
+
+}
---
+++ src/main/java/com/plataforma3d/plataforma3D2024/configuration/security/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.plataforma3d.plataforma3D2024.configuration.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+}
