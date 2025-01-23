a151fbcfe37f978a44c71ab070e9e6ba461e0508
Keny Pau Vas <keny_pauccarima@usmp.pe>
2025-01-23 11:36:15 -0500

    fix cors

--- src/main/java/com/plataforma3d/plataforma3D2024/configuration/CorsConfig.java
+++ src/main/java/com/plataforma3d/plataforma3D2024/configuration/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig implements WebMvcConfigurer{
                 .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","ANY")
                 .allowedHeaders("*")
+                .exposedHeaders("Authorization")
                 .allowCredentials(true);
 
     }
