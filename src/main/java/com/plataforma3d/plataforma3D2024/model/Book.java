8a47b8fae12363d94264213c0285b23df554f319
Keny Pau Vas <keny_pauccarima@usmp.pe>
2025-03-24 12:14:28 -0500

    Asignación de Unidades de aprendizaje segun el plan de estudios del colegio

--- src/main/java/com/plataforma3d/plataforma3D2024/model/Book.java
+++ src/main/java/com/plataforma3d/plataforma3D2024/model/Book.java
@@ -1,5 +1,7 @@
 package com.plataforma3d.plataforma3D2024.model;
 
+import com.plataforma3d.plataforma3D2024.model.utilitario.RoleEnum;
+import com.plataforma3d.plataforma3D2024.model.utilitario.UnidadAprendizaje;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -15,6 +17,10 @@ public class Book {
 
     private String title;
     private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "unidad_aprendizaje")
+    private UnidadAprendizaje unidadAprendizaje;
 
     private String coverPath;
     private String filePath;
